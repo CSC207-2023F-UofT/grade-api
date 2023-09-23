abf1b900cf92bea574c9af8f0e75de75b3027c41
Jonathan Calver <calver@cs.toronto.edu>
2023-09-23 12:19:51 -0400

    removed solutions

--- src/main/java/use_case/GetAverageGradeUseCase.java
+++ src/main/java/use_case/GetAverageGradeUseCase.java
@@ -12,16 +12,6 @@ public final class GetAverageGradeUseCase {
 
     public float getAverageGrade(String course) {
         // TODO: Get average grade for all students in your team.
-
-        float total = 0;
-        Team team = gradeDB.getMyTeam();
-        for (String s:
-             team.getMembers()) {
-            Grade g = gradeDB.getGrade(s, course);
-
-            total += g.getGrade();
-        }
-        return total / team.getMembers().length;
-
+        return 0.0f;
     }
 }
