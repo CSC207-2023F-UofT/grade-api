ff2f3a52eabb1c27bb1bd9b9547fdca16b494e3e
jvenchy <joshshergill@gmail.com>
2023-09-25 16:13:32 -0400

    implemented getAverageGrade

--- src/main/java/use_case/GetAverageGradeUseCase.java
+++ src/main/java/use_case/GetAverageGradeUseCase.java
@@ -11,7 +11,13 @@ public final class GetAverageGradeUseCase {
     }
 
     public float getAverageGrade(String course) {
-        // TODO: Get average grade for all students in your team.
-        return 0.0f;
+        Team t = gradeDB.getMyTeam();
+        String[] members = t.getMembers();
+        float gradeSum = 0;
+
+        for (String member : members) {
+            gradeSum += gradeDB.getGrade(member, course).getGrade();
+        }
+        return gradeSum / members.length;
     }
 }
